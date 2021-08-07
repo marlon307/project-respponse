b282a665382ec2c9118f4633dd3a212fd9036570
marlon307 <marlon-ramosb@hotmail.com>
2021-08-07 13:59:44 -0300

    Start Header

---
+++ components/Header/Header.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import style from './styles/styleHeader.module.scss';
+
+function Header() {
+  return (
+    <header className={ style.header }>
+      teste
+    </header>
+  );
+}
+
+export default Header;
