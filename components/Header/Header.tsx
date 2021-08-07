4cbcacbe9f4675f28433ee660ad9850cd57b064f
marlon307 <marlon-ramosb@hotmail.com>
2021-08-07 16:03:33 -0300

    Logo menu mobile

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -1,10 +1,21 @@
 import React from 'react';
+import Image from 'next/image';
 import style from './styles/styleHeader.module.scss';
+import Logo from '../../img/Logo.svg';
+import Menu from '../../img/Menu.svg';
 
 function Header() {
   return (
     <header className={ style.header }>
-      teste
+      <div className={ style.container }>
+        <Image
+          src={ Logo }
+          alt="Respponse"
+        />
+      </div>
+      <div className={ style.menu }>
+        <Image src={ Menu } alt="Menu" />
+      </div>
     </header>
   );
 }
