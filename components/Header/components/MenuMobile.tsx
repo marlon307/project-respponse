c06fa2870de2727d5900b2ce780b143c8307935a
marlon307 <marlon-ramosb@hotmail.com>
2021-08-11 13:41:16 -0300

    Menu mobilet update function open exit

--- components/Header/components/MenuMobile.tsx
+++ components/Header/components/MenuMobile.tsx
@@ -1,5 +1,6 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Image from 'next/image';
+import cx from 'classnames';
 import Menu from '../../../assets/img/Menu.svg';
 import icoFav from '../../../assets/img/heart.svg';
 import icoAcount from '../../../assets/img/setting.svg';
@@ -11,35 +12,48 @@ import style from './styles/styleMenuMobile.module.scss';
 import Bar from '../../SearchBar/Bar';
 
 function MenuMobile() {
+  const [dropMnMobile, setDropMnMobile] = useState(false);
   return (
     <div className={ style.mobile }>
-      <Image src={ Menu } alt="Menu" />
-      <div className={ style.dropmobile }>
-        <Bar />
-        <ul>
-          <li>
-            <Image src={ icoHelp } />
-            Ajuda
-          </li>
-          <li>
-            <Image src={ icoAcount } />
-            Conta
-          </li>
-          <li>
-            <Image src={ icoFav } />
-            Favoritos
-          </li>
-          <li>
-            <Image src={ iconBag } />
-            Sacola
-          </li>
-          <li>
-            <Image src={ iconLogin } />
-            Login
-          </li>
-        </ul>
-        <div className={ style.close }>
-          <Image src={ iconClose } />
+      <Image
+        src={ Menu }
+        alt="Menu"
+        onClick={ () => setDropMnMobile(true) }
+      />
+      <div className={ cx(style.dromn, {
+        [style.drop]: dropMnMobile,
+      }) }
+      >
+        <div className={ style.dropmobile }>
+          <Bar />
+          <ul>
+            <li>
+              <Image src={ icoHelp } />
+              Ajuda
+            </li>
+            <li>
+              <Image src={ icoAcount } />
+              Conta
+            </li>
+            <li>
+              <Image src={ icoFav } />
+              Favoritos
+            </li>
+            <li>
+              <Image src={ iconBag } />
+              Sacola
+            </li>
+            <li>
+              <Image src={ iconLogin } />
+              Login
+            </li>
+          </ul>
+          <div className={ style.close }>
+            <Image
+              src={ iconClose }
+              onClick={ () => setDropMnMobile(false) }
+            />
+          </div>
         </div>
       </div>
     </div>
