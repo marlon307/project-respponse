6631fbb4bae71f4d414a452b27c7d9adda8b6645
marlon307 <marlon-ramosb@hotmail.com>
2021-08-12 11:23:11 -0300

    Page user settings

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,26 @@
 import React from 'react';
+import Image from 'next/image';
+import style from './styles/styleIndex.module.scss';
+import TesteSlide from '../assets/img/mWYhrOiAgmA.png';
 
 function index() {
   return (
-    <div>MainPage</div>
+    <>
+      <section className={ style.slide }>
+        <Image src={ TesteSlide } />
+      </section>
+      <section className={ style.sectionfilter }>
+        <div className={ style.category }>
+          Categoria
+        </div>
+        <div className={ style.filter }>
+          Filter
+        </div>
+      </section>
+      <section className={ style.advertising }>
+        Divugaçoes
+      </section>
+    </>
   );
 }
 
--- pages/resetpsw.tsx
+++ pages/resetpsw.tsx
@@ -1,9 +1,21 @@
 import React from 'react';
+import Link from 'next/link';
+import Input from '../components/ComponentsForm/Input';
+import style from './styles/styleLogin.module.scss';
 
 function resetpsw() {
   return (
-    <div>
-      Recuperar login
+    <div className={ style.contlogin }>
+      <h1>Recuperar senha</h1>
+      <form>
+        <div className="inputs">
+          <Input id="email" type="email" name="email" placeholder="E-mail" />
+        </div>
+        <div className={ style.action }>
+          <Link href="/login">Fazer Login</Link>
+          <button type="button">Enviar</button>
+        </div>
+      </form>
     </div>
   );
 }
