c208ca2c8f09a0f0fd4bbf2163ddf1ff9c0bd32f
marlon307 <marlon-ramosb@hotmail.com>
2021-08-12 09:31:10 -0300

    Login Register update

--- pages/login.tsx
+++ pages/login.tsx
@@ -6,20 +6,24 @@ import style from './styles/styleLogin.module.scss';
 function login() {
   return (
     <div className={ style.contlogin }>
-      <h1>Login</h1>
+      <h1>Entrar</h1>
       <form>
-        <Input id="email" type="email" name="email" placeholder="E-mail" />
-        <Input id="psw" type="password" name="psw" placeholder="Senha" />
-        <Link href="/resetpsw">Esqueceu a senha?</Link>
+        <div className="inputs">
+          <Input id="email" type="email" name="email" placeholder="E-mail" />
+          <Input id="psw" type="password" name="psw" placeholder="Senha" />
+        </div>
         <div className={ style.action }>
           <label htmlFor="remember">
             <input id="remember" type="checkbox" />
             { ' ' }
             Lembrar meus dados.
           </label>
+          <Link href="/resetpsw">Esqueceu a senha?</Link>
+        </div>
+        <div className={ style.action }>
+          <Link href="/register">Criar conta</Link>
           <button type="button">Entrar</button>
         </div>
-        <Link href="/register">Criar conta</Link>
       </form>
     </div>
   );
