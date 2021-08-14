0c0f4893858e4b09f40f758e1f07886b375ef039
marlon307 <marlon-ramosb@hotmail.com>
2021-08-14 09:07:41 -0300

    Page bag

--- pages/favorite.tsx
+++ pages/favorite.tsx
@@ -2,13 +2,13 @@ import React from 'react';
 import Image from 'next/image';
 import { SmallCard } from '../components/Cards';
 import style from './styles/styleFavorite.module.scss';
-import iconBag from '../assets/img/u_heart-sign.svg';
+import iconFav from '../assets/img/u_heart-sign.svg';
 
 function favorite() {
   return (
     <div className={ style.favorites }>
       <h2 className={ style.title } title="Favoritos">
-        <Image src={ iconBag } />
+        <Image src={ iconFav } />
         Favoritos
       </h2>
       <ul>
