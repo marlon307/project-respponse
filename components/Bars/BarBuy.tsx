9940cfc7e9525026527b9e35312ef151f62dbcca
marlon307 <marlon-ramosb@hotmail.com>
2021-08-17 09:22:12 -0300

    Page bag bar price update designe

--- components/Bars/BarBuy.tsx
+++ components/Bars/BarBuy.tsx
@@ -1,10 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
+import cx from 'classnames';
 import { BuyFinishBtn } from '../Buttons';
 import style from './styleBarBuy.module.scss';
 
 function BarBuy() {
+  const [openInfo, setOpenInfo] = useState(false);
+
+  function openBarMenu(event: { preventDefault: () => void; }) {
+    event.preventDefault();
+    setOpenInfo(!openInfo);
+  }
+
   return (
-    <section className={ style.buybar }>
+    <section className={
+      cx(style.buybar, { [style.open]: openInfo })
+    }
+    >
       <div className={ style.container }>
         <div className={ style.calc }>
           <div>
@@ -28,6 +39,7 @@ function BarBuy() {
           <BuyFinishBtn />
         </div>
       </div>
+      <a href="/" onClick={ openBarMenu }> </a>
     </section>
   );
 }
