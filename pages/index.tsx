02f4cb8246b59de1c294cb8836eae038a934e6dc
marlon307 <marlon-ramosb@hotmail.com>
2021-08-23 12:00:52 -0300

    Page creat filter

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,6 +6,7 @@ import TesteSlide from '../assets/img/mWYhrOiAgmA.png';
 import { CardCategory } from '../components/Cards';
 import { mockApiCategory } from '../service/colorsMock';
 import { BtnPrevNext } from '../components/Buttons';
+import BarFilter from '../components/Bars/BarFilter';
 
 function index() {
   const buttonPrevNext = createRef<Flicking>();
@@ -30,9 +31,7 @@ function index() {
           </Flicking>
           <BtnPrevNext reference={ buttonPrevNext } typePrevOrNext="next" />
         </div>
-        <div className={ style.filter }>
-          Filter
-        </div>
+        <BarFilter />
       </section>
       <section className={ style.advertising }>
         Divugaçoes
