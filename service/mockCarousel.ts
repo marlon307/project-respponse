d7ee299393ffb2e36ee4e8e302203d67e437a952
marlon307 <marlon-ramosb@hotmail.com>
2021-08-25 08:02:30 -0300

    Page home carrousel priority image add mock

---
+++ service/mockCarousel.ts
@@ -0,0 +1,23 @@
+import TesteSlide1 from '../assets/img/mWYhrOiAgmA.png';
+import TesteSlide2 from '../assets/img/SmIlY2uAHo8.png';
+import TesteSlide3 from '../assets/img/atikh-bana-_KaMTEmJnxY-unsplash.jpg';
+
+const mockCarousel = [
+  {
+    id: '0',
+    urlImg: TesteSlide1,
+    url: '',
+  },
+  {
+    id: '2',
+    urlImg: TesteSlide2,
+    url: '',
+  },
+  {
+    id: '3',
+    urlImg: TesteSlide3,
+    url: '',
+  },
+];
+
+export default mockCarousel;
