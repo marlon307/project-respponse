3f2365125e9072f6588b03354c5b277751a0f89c
marlon307 <marlon-ramosb@hotmail.com>
2021-08-29 09:01:16 -0300

    Update scroll passive

--- hooks/useScroll.tsx
+++ hooks/useScroll.tsx
@@ -1,5 +1,4 @@
 import { useEffect, useState, useCallback } from 'react';
-import { supportsPassiveEvents } from 'detect-passive-events';
 
 function useScroll() {
   const [scrollY, setScrollY] = useState(0);
@@ -9,11 +8,8 @@ function useScroll() {
   );
 
   useEffect(() => {
-    if (supportsPassiveEvents) {
-      window.addEventListener('scroll', functionScroll, { capture: false, passive: true });
-    } else {
-      window.addEventListener('scroll', functionScroll, false);
-    }
+    window.addEventListener('scroll', functionScroll, { capture: false, passive: true });
+
     return () => {
       window.removeEventListener('scroll', functionScroll);
     };
