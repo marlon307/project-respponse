2a40a2f1e62454852eb7fba02e8e674a62623f14
marlon307 <marlon-ramosb@hotmail.com>
2021-09-01 09:43:12 -0300

    Pwa update 7

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -18,7 +18,7 @@ function MyApp({ Component, pageProps }: AppProps) {
         <link rel="manifest" href="/manifest.json" />
         <meta name="theme-color" content="#FFFFFF" />
         <link rel="apple-touch-icon" href="/favico.ico" />
-        <meta name="viewport" content="minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no, user-scalable=no, viewport-fit=cover" />
+        <meta name="viewport" content="minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no, user-scalable=no, viewport-fit=cover" user-scalable="no" />
       </Head>
       <Header />
       <Component { ...pageProps } />
