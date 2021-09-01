4dcc4ffee1738b5146159a270b79de4e17ba84ff
marlon307 <marlon-ramosb@hotmail.com>
2021-09-01 09:19:01 -0300

    Pwa update 4

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -15,7 +15,8 @@ function MyApp({ Component, pageProps }: AppProps) {
         <meta name="description" content="Respponse loja de roupas e acessórios para o dia a dia, tudo de melhor qualidade para você." />
         <link rel="apple-touch-icon" href="/favico.ico" />
         <link rel="icon" type="image/png" sizes="32x32" href="./favico.ico" />
-        <link rel="manifest" href="/manifest.json" />
+        <link rel="manifest" href="/static/manifest.json" />
+        <link rel="apple-touch-icon" href="/static/icons/favico.ico" />
         <meta name="viewport" content="minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no, user-scalable=no, viewport-fit=cover" />
       </Head>
       <Header />
