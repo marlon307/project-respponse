f7dc918cdea34f05dc065e37dd3f3dc822bfe997
marlon307 <marlon-ramosb@hotmail.com>
2021-09-09 10:56:35 -0300

    Pg order finish

--- pages/order/[orderId].tsx
+++ pages/order/[orderId].tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { SmallCard } from '../../components/Cards';
 import style from './style.module.scss';
 
 function orderId() {
@@ -33,6 +34,49 @@ function orderId() {
             <span title="Em Transporte" aria-label="Em Transporte" />
             <span title="Pedido Entregue" aria-label="Pedido Entregue" />
           </div>
+        </div>
+      </div>
+      <div className={ style.orderadders }>
+        <div>
+          <h3>Dados da entrega</h3>
+          <span>Rua São Paulo, 000 - Santa Cruz </span>
+          <span>Casa</span>
+          <span>Valença, MG 00000-000</span>
+        </div>
+        <div>
+          <h3>Correios</h3>
+          <span>QF000000000BR</span>
+        </div>
+      </div>
+      <div className={ style.orderitems }>
+        <ul>
+          <li>
+            <SmallCard />
+          </li>
+          <li>
+            <SmallCard />
+          </li>
+          <li>
+            <SmallCard />
+          </li>
+        </ul>
+      </div>
+      <div className={ style.footerTotal }>
+        <div>
+          <span>Total Dos produtos</span>
+          <span>R$ 110,99</span>
+        </div>
+        <div>
+          <span>Frete</span>
+          <span>R$ 15,00</span>
+        </div>
+        <div>
+          <span>Descontos</span>
+          <span>R$ 0,00</span>
+        </div>
+        <div>
+          <span>Total</span>
+          <span>R$ 125,99</span>
         </div>
       </div>
     </div>
