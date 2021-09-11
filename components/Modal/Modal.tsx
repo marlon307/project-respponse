5baf4e5680a65ad83e88ef766baa42f2a01c156c
marlon307 <marlon-ramosb@hotmail.com>
2021-09-11 10:44:57 -0300

    Modal open order modal

--- components/Modal/Modal.tsx
+++ components/Modal/Modal.tsx
@@ -1,21 +1,11 @@
 import React from 'react';
-// { ReactNode }
-import cx from 'classnames';
 import style from './style.module.scss';
-import stateModal from './stateModal';
 
 function Modal() {
-  const { openModal } = stateModal();
-
   return (
     <div
       id="modal"
-      className={
-        cx(
-          style.modal,
-          { [style.open]: openModal },
-        )
-      }
+      className={ style.modal }
     />
   );
 }
