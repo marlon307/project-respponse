4c04391c28b1beb7fbaf2cdbc0fde25a1bbeaa39
marlon307 <marlon-ramosb@hotmail.com>
2021-09-15 14:43:54 -0300

    Update Inputs autocomplete 2

--- pages/usercfg.tsx
+++ pages/usercfg.tsx
@@ -10,11 +10,11 @@ function usercfg() {
         <div className="inputs">
           <form>
             <h3>Informações básicas</h3>
-            <Input id="name" type="name" name="name" placeholder="Nome" />
-            <Input id="email" type="email" name="email" placeholder="E-mail" />
-            <Input id="psw" type="password" name="psw" placeholder="Senha" />
-            <Input id="date" type="date" name="date" placeholder="Data" />
-            <Input id="doc" type="doc" name="doc" placeholder="CPF" />
+            <Input id="name" type="name" name="name" placeHolder="Nome" />
+            <Input id="email" type="email" name="email" placeHolder="E-mail" />
+            <Input id="psw" type="password" name="psw" placeHolder="Senha" />
+            <Input id="date" type="date" name="date" placeHolder="Data" />
+            <Input id="doc" type="doc" name="doc" placeHolder="CPF" />
             <div className={ style.genere }>
               <span>Sexo</span>
               <div>
@@ -25,8 +25,8 @@ function usercfg() {
             </div>
             <div className={ style.contact }>
               <span>Contato</span>
-              <Input id="tel" type="tel" name="tel" placeholder="Telefone" />
-              <Input id="cel" type="tel" name="cel" placeholder="Telefone" />
+              <Input id="tel" type="tel" name="tel" placeHolder="Telefone" />
+              <Input id="cel" type="tel" name="cel" placeHolder="Telefone" />
             </div>
           </form>
         </div>
