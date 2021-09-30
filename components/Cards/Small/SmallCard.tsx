b55a3dd5be4b1f717681a58f007171594773e548
marlon307 <marlon-ramosb@hotmail.com>
2021-09-30 14:54:14 -0300

    Update Edit modal add

--- components/Cards/Small/SmallCard.tsx
+++ components/Cards/Small/SmallCard.tsx
@@ -1,8 +1,10 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Image from 'next/image';
 import TesteImgUrl from '../../../assets/img/brian-lawson-e9o9sAy5PL4-unsplash 1.png';
 import style from './stylesSmallCard.module.scss';
 import Svg from '../../../assets/Svg';
+import ContentModal from '../../Modal/ContentModal';
+import CardEdit from '../CardEdit/CardEdit';
 
 type PSmallCard = {
   removable?: boolean
@@ -10,46 +12,66 @@ type PSmallCard = {
 }
 
 function SmallCard({ removable, editable }: PSmallCard) {
+  const [opeModal, setOpenModal] = useState(false);
+
+  function openModalEdit(event: { preventDefault: () => void; }) {
+    event.preventDefault();
+    setOpenModal(true);
+  }
+
   return (
-    <div className={ style.smallcard }>
-      <div className={ style.img }>
-        <figure>
-          <Image
-            src={ TesteImgUrl }
-            alt="Title"
-            quality="90"
-            placeholder="blur"
-            layout="responsive"
-            width={ 130 }
-            height={ 165 }
-          />
-        </figure>
-      </div>
-      <div className={ style.desc }>
-        <h2>Berrylush</h2>
-        <h3>Top Forever 21 Canelado Preto</h3>
-        <div className={ style.infos }>
-          <div className={ style.setting }>
-            <span title="Roxo Claro" style={ { background: '#AAB4D9' } } />
-            <span title="Tamanho G" style={ { color: '#AAB4D9' } }>G</span>
-            <span title="3 Berrylush - Top Forever 21 Canelado Preto" style={ { color: '#AAB4D9' } }>3x</span>
-            { editable && (
-              <a href="/" aria-label="Editar quantidade e cor." title="Editar quantidade e cor">
-                <Svg icoName="edit" />
-              </a>
-            ) }
-          </div>
-          <div className={ style.price }>
-            <span title="Valor da unidade R$ 199,90">R$ 199,90</span>
+    <>
+      <div className={ style.smallcard }>
+        <div className={ style.img }>
+          <figure>
+            <Image
+              src={ TesteImgUrl }
+              alt="Title"
+              quality="90"
+              placeholder="blur"
+              layout="responsive"
+              width={ 130 }
+              height={ 165 }
+            />
+          </figure>
+        </div>
+        <div className={ style.desc }>
+          <h2>Berrylush</h2>
+          <h3>Top Forever 21 Canelado Preto</h3>
+          <div className={ style.infos }>
+            <div className={ style.setting }>
+              <span title="Roxo Claro" style={ { background: '#AAB4D9' } } />
+              <span title="Tamanho G" style={ { color: '#AAB4D9' } }>G</span>
+              <span title="3 Berrylush - Top Forever 21 Canelado Preto" style={ { color: '#AAB4D9' } }>3x</span>
+              { editable && (
+                <a
+                  href="/"
+                  aria-label="Editar quantidade e cor."
+                  title="Editar quantidade e cor"
+                  onClick={ openModalEdit }
+                >
+                  <Svg icoName="edit" />
+                </a>
+              ) }
+            </div>
+            <div className={ style.price }>
+              <span title="Valor da unidade R$ 199,90">R$ 199,90</span>
+            </div>
           </div>
         </div>
+        { removable && (
+          <div className="action" title="Excluir">
+            <Svg icoName="trash" />
+          </div>
+        ) }
       </div>
-      { removable && (
-        <div className="action" title="Excluir">
-          <Svg icoName="trash" />
-        </div>
-      ) }
-    </div>
+      <ContentModal
+        isOpen={ opeModal }
+        openModal={ setOpenModal }
+      >
+        { opeModal && <CardEdit /> }
+      </ContentModal>
+    </>
   );
 }
 
