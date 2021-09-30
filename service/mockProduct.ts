213632082ca1773579647920a27d0ff5e20fadb4
marlon307 <marlon-ramosb@hotmail.com>
2021-09-30 08:16:25 -0300

    Img mock product

---
+++ service/mockProduct.ts
@@ -0,0 +1,32 @@
+import imageteste from '../assets/img/brian-lawson-a-mtphgCGo8-unsplash_1-removebg-preview.png';
+import imageteste1 from '../assets/img/brian-lawson-e9o9sAy5PL4-unsplash_1-removebg-preview.png';
+
+const mockProduct = [
+  {
+    id: 0,
+    img: imageteste,
+    title: 'Title',
+  },
+  {
+    id: 1,
+    img: imageteste1,
+    title: 'Title',
+  },
+  {
+    id: 2,
+    img: imageteste,
+    title: 'Title',
+  },
+  {
+    id: 3,
+    img: imageteste1,
+    title: 'Title',
+  },
+  {
+    id: 4,
+    img: imageteste,
+    title: 'Title',
+  },
+];
+
+export default mockProduct;
