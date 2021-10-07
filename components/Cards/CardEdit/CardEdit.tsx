8d5e3aecd3ec13e61ec70098afb57b3b883af9d5
marlon307 <marlon-ramosb@hotmail.com>
2021-10-07 19:52:18 -0300

    Add loading image

--- components/Cards/CardEdit/CardEdit.tsx
+++ components/Cards/CardEdit/CardEdit.tsx
@@ -1,25 +1,18 @@
 import React from 'react';
-import Image from 'next/image';
 import BarColors from '../../Bars/BarColors';
 import Qtd from '../../Bars/Qtd';
 import style from './style.module.scss';
-import img from '../../../assets/img/brian-lawson-a-mtphgCGo8-unsplash_1-removebg-preview.png';
 import BarSize from '../../Bars/BarSize';
 import mockColors from '../../../service/colorsMock';
+import LoadingImage from '../../LoadImage/LoadingImage';
+import img from '../../../assets/img/brian-lawson-e9o9sAy5PL4-unsplash_1-removebg-preview.png';
 
 function CardEdit() {
   return (
     <div className={ style.edit }>
       <div className={ style.visualcont }>
         <div className={ style.contimg }>
-          <Image
-            quality={ 90 }
-            placeholder="blur"
-            src={ img }
-            alt="Title"
-            layout="responsive"
-            objectPosition="relative"
-          />
+          <LoadingImage url={ img } />
         </div>
         <BarColors array={ mockColors } />
       </div>
