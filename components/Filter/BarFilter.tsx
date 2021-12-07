fefec63bdaee3c24c3d8a6e5172df54bde4f3389
marlon307 <marlon-ramosb@hotmail.com>
2021-12-07 08:46:31 -0300

    Update filter resize

--- components/Filter/BarFilter.tsx
+++ components/Filter/BarFilter.tsx
@@ -1,7 +1,9 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import dynamic from 'next/dynamic';
 import Loading from '../Loading/Loading';
 import style from './style.module.scss';
+import useWindowSize from '../../hooks/useWindowSize';
+import ContentModal from '../Modal/ContentModal';
 
 const Filter = dynamic(() => import('./Filter'), {
   loading: () => <Loading />,
@@ -11,10 +13,39 @@ const Filter = dynamic(() => import('./Filter'), {
 // });
 
 const BarFilter = function BarFilter() {
+  const [sizeWidth] = useWindowSize();
+  const [modalFilter, setModalFilter] = useState(false);
+
+  useEffect(() => {
+    if (sizeWidth > 790) {
+      setModalFilter(false);
+    }
+  }, [sizeWidth]);
+
+  function handleClick() {
+    setModalFilter(!modalFilter);
+  }
+
   return (
     <div className={ style.barfilter }>
       {/* <OrderFilter /> */ }
-      <Filter />
+      { sizeWidth > 790
+        ? <Filter />
+        : (
+          <button
+            aria-label="Filtro"
+            type="button"
+            onClick={ handleClick }
+          >
+            <h3>Filtro</h3>
+          </button>
+        ) }
+      <ContentModal
+        isOpen={ modalFilter }
+        openModal={ setModalFilter }
+      >
+        { modalFilter && <Filter /> }
+      </ContentModal>
     </div>
   );
 };
