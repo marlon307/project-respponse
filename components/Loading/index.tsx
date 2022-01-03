4017fd2a66733f133ba5f28bfd3560639ae90599
marlon307 <marlon-ramosb@hotmail.com>
2022-01-03 17:29:46 -0300

    Rename file loading

---
+++ components/Loading/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Loading = function Loading() {
+  return (
+    <div className="spinner">
+      <div className="double-bounce1" />
+      <div className="double-bounce2" />
+    </div>
+  );
+};
+
+export default Loading;
