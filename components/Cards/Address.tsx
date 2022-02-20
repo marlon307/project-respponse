66713a12bc979079287f25101eaed6deec40d621
marlon307 <marlon-ramosb@hotmail.com>
2022-02-20 13:15:03 -0300

    Update styles cards Pay and addres

--- components/Cards/Address.tsx
+++ components/Cards/Address.tsx
@@ -1,29 +1,34 @@
 import React from 'react';
+import style from './CardAddress/style.module.scss';
 import { CardAdderess } from '.';
 
 function Address() {
   return (
     <>
-      <CardAdderess
-        name="Name Teste"
-        road="Fernando de noronha"
-        number="123"
-        city="Ipatinga"
-        uf="MG"
-        zipcode="12345-67"
-        district="Alterosas"
-        removable
-      />
-      <CardAdderess
-        name="Name Teste"
-        road="Fernando de noronha"
-        number="123"
-        city="Ipatinga"
-        uf="MG"
-        zipcode="12345-67"
-        district="Alterosas"
-        removable
-      />
+      <div className={ style.address }>
+        <CardAdderess
+          name="Name Teste"
+          road="Fernando de noronha"
+          number="123"
+          city="Ipatinga"
+          uf="MG"
+          zipcode="12345-67"
+          district="Alterosas"
+          removable
+        />
+      </div>
+      <div className={ style.address }>
+        <CardAdderess
+          name="Name Teste"
+          road="Fernando de noronha"
+          number="123"
+          city="Ipatinga"
+          uf="MG"
+          zipcode="12345-67"
+          district="Alterosas"
+          removable
+        />
+      </div>
     </>
   );
 }
