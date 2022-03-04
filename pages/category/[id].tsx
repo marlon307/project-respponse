b31a1b0946d51001f7535fd53d5c52b2e96e26dd
marlon307 <marlon-ramosb@hotmail.com>
2022-03-04 19:28:19 -0300

    Add typing statics pgs

--- pages/category/[id].tsx
+++ pages/category/[id].tsx
@@ -3,6 +3,7 @@ import { CardProduct } from 'components/Cards/CardProduct';
 import { BarFilter } from 'components/Filter';
 import { mockminObjectCards2 } from 'service/mockCards';
 import mockCategory from 'service/mockCategory';
+import { GetStaticProps, GetStaticPaths } from 'next';
 import style from './style.module.scss';
 
 function categoryId() {
@@ -22,14 +23,14 @@ function categoryId() {
 
 export default categoryId;
 
-export async function getStaticProps({ params }: any) {
+export const getStaticProps: GetStaticProps = async ({ params }: any) => {
   const pgProps = await mockCategory.find(({ path }) => path === params.id);
   return {
     props: { pgProps },
   };
-}
+};
 
-export async function getStaticPaths() {
+export const getStaticPaths: GetStaticPaths = async () => {
   const paths = await mockCategory
     .map(({ path }) => ({
       params: { id: path },
@@ -39,4 +40,4 @@ export async function getStaticPaths() {
     paths,
     fallback: false,
   };
-}
+};
