98af1f61978384025ad41b00ed97a2f1ce7ef284
marlon307 <marlon-ramosb@hotmail.com>
2022-04-11 15:04:26 -0300

    Update orders for table width

--- components/Order/Orders.tsx
+++ components/Order/Orders.tsx
@@ -11,34 +11,32 @@ function Orders({ execFunction }: TPropsOrders) {
   }, []);
 
   return (
-    <section className={ style.section }>
-      <table className={ style.table } cellSpacing="0">
-        <thead>
-          <tr>
-            <th>Produto</th>
-            <th>Loja</th>
-            <th>Preço</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr onClick={ () => orderIdOpen('00003') }>
-            <td>0003</td>
-            <td>01/01/22</td>
-            <td>Pag. Pedente</td>
-          </tr>
-          <tr onClick={ () => orderIdOpen('00002') }>
-            <td>0002</td>
-            <td>01/01/22</td>
-            <td>Enviado.</td>
-          </tr>
-          <tr onClick={ () => orderIdOpen('00001') }>
-            <td>0001</td>
-            <td>01/01/22</td>
-            <td>Entregue</td>
-          </tr>
-        </tbody>
-      </table>
-    </section>
+    <table className={ style.table } cellSpacing="0">
+      <thead>
+        <tr>
+          <th>Produto</th>
+          <th>Loja</th>
+          <th>Preço</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr onClick={ () => orderIdOpen('00003') }>
+          <td>0003</td>
+          <td>01/01/22</td>
+          <td>Pag. Pedente</td>
+        </tr>
+        <tr onClick={ () => orderIdOpen('00002') }>
+          <td>0002</td>
+          <td>01/01/22</td>
+          <td>Enviado.</td>
+        </tr>
+        <tr onClick={ () => orderIdOpen('00001') }>
+          <td>0001</td>
+          <td>01/01/22</td>
+          <td>Entregue</td>
+        </tr>
+      </tbody>
+    </table>
   );
 }
 
