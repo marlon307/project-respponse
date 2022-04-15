c94284b5332f8b0978783ad0d014a61065c0d18c
marlon307 <marlon-ramosb@hotmail.com>
2022-04-15 20:23:25 -0300

    updates images ctg

--- service/mockCategory.ts
+++ service/mockCategory.ts
@@ -1,14 +1,15 @@
-import imgMasculino from '../assets/img/cibelly-roberta-SE4Xc1WvIkU-unsplash-removebg-preview.png';
+import imgMasculino from '../assets/img/jonas-jaeken-31upv631160-unsplash_1-removebg-preview.png';
 import imgCriança from '../assets/img/moses-vega-_YfoApRxd4I-unsplash-removebg-preview.png';
 import imgBolsa from '../assets/img/ugluk-potroshitel-XwjrPFW7xw0-unsplash-removebg-preview.png';
 import imgPerfumes from '../assets/img/parth-natani-uqJdOfHGb-w-unsplash-removebg-preview.png';
 import imgRelogios from '../assets/img/jess-bailey-DXd6X3wuRA0-unsplash-removebg-preview.png';
-import imgFemenino from '../assets/img/revolt-164_6wVEHfI-unsplash-removebg-preview.png';
+import imgTenis from '../assets/img/revolt-164_6wVEHfI-unsplash-removebg-preview.png';
+import imgFemenino from '../assets/img/ian-dooley-iGh7qbW9kUM-unsplash_1-removebg-preview.png';
 
 const mockCategory = [
   {
     ctgID: 0,
-    imgCategory: imgFemenino,
+    imgCategory: imgTenis,
     categoryName: 'Tênis',
     subTitle: 'Tênis',
     path: 'Tênis',
@@ -33,7 +34,7 @@ const mockCategory = [
     categoryName: 'Bolsa',
     subTitle: 'Bolsa',
     path: 'bolsa',
-    color: '#9c5428',
+    color: '#ff6100',
   }, {
     ctgID: 5,
     imgCategory: imgPerfumes,
@@ -48,6 +49,13 @@ const mockCategory = [
     subTitle: 'Relógios',
     path: 'relogios',
     color: '#5dada0',
+  }, {
+    ctgID: 7,
+    imgCategory: imgFemenino,
+    categoryName: 'Femenino',
+    subTitle: 'Femenino',
+    path: 'femenino',
+    color: '#e724e1',
   },
 ];
 
