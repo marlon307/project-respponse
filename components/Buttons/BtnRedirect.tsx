1b5c577a4703bac77c8dd372560297db70c8446b
marlon307 <marlon-ramosb@hotmail.com>
2022-04-21 14:44:25 -0300

    update style pg error

--- components/Buttons/BtnRedirect.tsx
+++ components/Buttons/BtnRedirect.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import cx from 'classnames';
 import Link from 'next/link';
 import Svg from '../../assets/Svg';
 import style from './style.module.scss';
@@ -12,7 +11,7 @@ type PBtnR = {
 function BtnRedirect({ path, titleBtn }: PBtnR) {
   return (
     <Link href={ path }>
-      <a className={ cx('button1', style.redirect) }>
+      <a className={ style.redirect }>
         <span>{ titleBtn }</span>
         <Svg icoName="indication" />
       </a>
