2b8bd4b1b7fe7555f719419ffdcf1617ad1b3607
marlon307 <marlon-ramosb@hotmail.com>
2022-04-21 10:43:55 -0300

    Add banner slide

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,22 +3,37 @@ import { CardCategory } from '../components/Cards';
 // import type { NextPage } from 'next';
 import style from '../Sass/style.module.scss';
 import mockCategory from '../service/mockCategory';
+import slide from '../assets/img/revolt-164_6wVEHfI-unsplash.jpg';
+import LoadingImage from '../components/LoadImage';
 
 function Home() {
   return (
     <main className={ style.main }>
+      <div className={ style.banner }>
+        <LoadingImage
+          src={ slide }
+          quality={ 85 }
+          alt="title"
+          width={ 1080 }
+          height={ 700 }
+          layout="fill"
+        />
+      </div>
       <div className={ style.listctg }>
-        { mockCategory.map(({
-          ctgID, imgCategory, categoryName, color, path,
-        }) => (
-          <CardCategory
-            key={ ctgID }
-            image={ imgCategory }
-            ctgName={ categoryName }
-            path={ path }
-            color={ color! }
-          />
-        )) }
+        <h2>Categorias</h2>
+        <div className={ style.list }>
+          { mockCategory.map(({
+            ctgID, imgCategory, categoryName, color, path,
+          }) => (
+            <CardCategory
+              key={ ctgID }
+              image={ imgCategory }
+              ctgName={ categoryName }
+              path={ path }
+              color={ color! }
+            />
+          )) }
+        </div>
       </div>
     </main>
   );
