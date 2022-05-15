02ee918a6e425700fa98bed3c1c012ab19515a04
marlon307 <marlon-ramosb@hotmail.com>
2022-05-15 15:26:02 -0300

    Category api

---
+++ pages/api/categorys.ts
@@ -0,0 +1,12 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import type { NextApiRequest, NextApiResponse } from 'next';
+import { categorys } from '../../service/mockCategory';
+
+export default function home(req: NextApiRequest, res: NextApiResponse<any>) {
+  if (req.method === 'GET') {
+    return res.status(200).json({
+      categorys,
+    });
+  }
+  return res.status(500).json({ message: 'Not found', status: 500 });
+}
