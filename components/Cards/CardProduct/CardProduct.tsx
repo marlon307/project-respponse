3fe7d5a3876b4d7354fd90533cd848d7dd18cfed
_marlon307 <marlon-ramosb@hotmail.com>
2022-05-15 14:50:14 -0300

    Card info

--- components/Cards/CardProduct/CardProduct.tsx
+++ components/Cards/CardProduct/CardProduct.tsx
@@ -31,14 +31,45 @@ function CardProduct({ id }: PCardP) {
           />
         </figure>
         <div className={ style.infocont }>
-          <CardInfo
-            type={ type }
-            title={ title }
-            price={ price }
-            colors={ options }
-            discount={ discount }
-            oldPrice={ oldPrice }
-          />
+          <div className={ style.info }>
+            <div className={ style.primaryline }>
+              <span>{ type }</span>
+              <div className={ style.colors }>
+                { colors!
+                  && colors.map((_null, index) => {
+                    const value = Object.values(colors![index]);
+                    return (
+                      <span
+                        key={ value[0] }
+                        title={ value[0] }
+                        style={ { backgroundColor: `${value[1]}` } }
+                      />
+                    );
+                  }) }
+              </div>
+            </div>
+            <div className={ style.secondline }>
+              <span>{ title }</span>
+            </div>
+            <div
+              className={ style.price }
+              data-oldprice={
+                discount > 0 ? oldPrice.toLocaleString('pt-br', {
+                  style: 'currency',
+                  currency: 'BRL',
+                }) : ''
+              }
+            >
+              <span>
+                {
+                  price.toLocaleString('pt-br', {
+                    style: 'currency',
+                    currency: 'BRL',
+                  })
+                }
+              </span>
+            </div>
+          </div>
         </div>
       </a>
     </Link>
