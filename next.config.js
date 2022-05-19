24910da6f6aec2a47d720e14b6cbb56c46583f33
_marlon307 <marlon-ramosb@hotmail.com>
2022-05-19 19:40:35 -0300

    infodesc margin

--- next.config.js
+++ next.config.js
@@ -4,11 +4,33 @@
 const withPWA = require('next-pwa');
 const runtimeCaching = require('next-pwa/cache');
 
+const headers = async () => {
+  return [
+    {
+      source: '/(.*)',
+      headers: [
+        {
+          key: 'X-Content-Type-Options',
+          value: 'nosniff'
+        },
+        {
+          key: 'X-Frame-Options',
+          value: 'SAMEORIGIN'
+        },
+        {
+          key: 'X-XSS-Protection',
+          value: '1; mode=block'
+        }
+      ],
+    },
+  ]
+}
+
 module.exports = withPWA({
   images: {
     domains: ['i.imgur.com'],
     deviceSizes: [280, 350, 640, 750, 828, 1080, 1200, 1920, 2048, 3840],
-    imageSizes: [70, 96, 100, 128, 130, 152, 220, 256, 300, 384, 1080, 1920],
+    imageSizes: [70, 96, 100, 128, 130, 256, 384, 1080, 1920],
   },
   i18n: {
     locales: ['pt-br'],
@@ -26,6 +48,8 @@ module.exports = withPWA({
     register: true,
     sw: 'sw.js',
   },
+  buildExcludes: [/middleware-manifest.json$/],
+  headers,
   serverRuntimeConfig: {
     PROJECT_ROOT: __dirname,
   },
