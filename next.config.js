518eefc3958422ca625f2c608e19c8052839ca75
marlon307 <marlon-ramosb@hotmail.com>
2022-05-20 07:50:28 -0300

    next config update

--- next.config.js
+++ next.config.js
@@ -4,28 +4,6 @@
 const withPWA = require('next-pwa');
 const runtimeCaching = require('next-pwa/cache');
 
-const headers = async () => {
-  return [
-    {
-      source: '/(.*)',
-      headers: [
-        {
-          key: 'X-Content-Type-Options',
-          value: 'nosniff'
-        },
-        {
-          key: 'X-Frame-Options',
-          value: 'SAMEORIGIN'
-        },
-        {
-          key: 'X-XSS-Protection',
-          value: '1; mode=block'
-        }
-      ],
-    },
-  ]
-}
-
 module.exports = withPWA({
   images: {
     domains: ['i.imgur.com'],
@@ -47,10 +25,5 @@ module.exports = withPWA({
     disable: process.env.NODE_ENV !== 'production',
     register: true,
     sw: 'sw.js',
-  },
-  buildExcludes: [/middleware-manifest.json$/],
-  headers,
-  serverRuntimeConfig: {
-    PROJECT_ROOT: __dirname,
   },
 });
