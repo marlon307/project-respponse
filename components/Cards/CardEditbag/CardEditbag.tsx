af8cf2a79dd2bc103842f33f82b7c2aa389b02fb
marlon307 <marlon-ramosb@hotmail.com>
2022-07-25 19:48:34 -0300

    New array select qtd

--- components/Cards/CardEditbag/CardEditbag.tsx
+++ components/Cards/CardEditbag/CardEditbag.tsx
@@ -1,15 +1,12 @@
 import React from 'react';
 import style from './style.module.scss';
 
-const limitsitem = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11,
-  12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30];
-
 function CardEditbag() {
   return (
     <div className={ style.cardedit }>
       <h2>Escolha a quantidade.</h2>
       <ul>
-        { limitsitem.map((value) => (
+        { [...Array(30).keys()].map((value) => (
           <li key={ value }>
             <button type="button">{ value }</button>
           </li>
