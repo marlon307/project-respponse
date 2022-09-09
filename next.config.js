7efdd98cb61cff09bf696647e823f7f442140e12
marlon307 <marlon-ramosb@hotmail.com>
2022-09-09 18:55:06 -0300

    Update manifet and remove device size

--- next.config.js
+++ next.config.js
@@ -15,7 +15,7 @@ const withPWA = require('next-pwa')({
 module.exports = withPWA({
   images: {
     domains: ['i.imgur.com'],
-    deviceSizes: [280, 350, 640, 750, 828, 1080, 1200, 1920, 2048, 3840],
+    deviceSizes: [280, 350, 640, 750, 828, 1080, 1200, 1920],
     imageSizes: [70, 96, 100, 128, 130, 256, 384, 1080, 1920],
   },
   i18n: {
