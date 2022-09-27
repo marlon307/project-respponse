12e7b3d6eb813b6238c31dbdf3e2f017c9fd1835
marlon307 <marlon-ramosb@hotmail.com>
2022-09-27 16:26:15 -0300

    optimize header

--- components/Header/components/MenuMobile.tsx
+++ components/Header/components/MenuMobile.tsx
@@ -43,8 +43,8 @@ function MenuMobile({ data }: any) {
           aria-label="Menu"
           onClick={ openMenu }
         >
-          <svg xmlns="http://www.w3.org/2000/svg" width="28" height="28" viewBox="0 0 24 24" fill="none">
-            <path d="M21 13.5H11a1 1 0 0 0 0 2h10a1 1 0 0 0 0-2Zm0-5H3a1 1 0 0 0 0 2h18a1 1 0 0 0 0-2Z" fill="#333333" />
+          <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
+            <path d="M21 13.5H11a1 1 0 0 0 0 2h10a1 1 0 0 0 0-2Zm0-5H3a1 1 0 0 0 0 2h18a1 1 0 0 0 0-2Z" fill="#333" />
           </svg>
         </button>
       ) : (
