eb156046972bedf427e0ecba8a2aaf84dc88ff98
marlon307 <marlon-ramosb@hotmail.com>
2022-09-27 15:29:29 -0300

    Mock api

---
+++ service/destaque_id.ts
@@ -0,0 +1,35 @@
+const destaque = {
+  category_name: 'Jaqueta',
+  color_list: [{
+    color: '#000',
+    color_name: 'Preto',
+    discount: 10,
+    id: 2,
+    price: 150,
+    url_image: 'https://i.imgur.com/ZsqzxjH.png',
+  }, {
+    color: '#54b1ff',
+    color_name: 'Azul',
+    discount: 0,
+    id: 1,
+    price: 176,
+    url_image: 'https://i.imgur.com/memTkIU.png',
+  }, {
+    color: '#e0635a',
+    color_name: 'Vermelho',
+    discount: 0,
+    id: 3,
+    price: 100,
+    url_image: 'https://i.imgur.com/lF3BZfu.png',
+  }, {
+    color: '#ffc107',
+    color_name: 'Laranja',
+    discount: 0,
+    id: 4,
+    price: 120,
+    url_image: 'https://i.imgur.com/2Onjm5S.png',
+  }],
+  id: 1,
+  title: 'Jeans station inverno',
+};
+export default destaque;
