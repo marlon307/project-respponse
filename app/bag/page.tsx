2781948fd59ec4363d24e7c282c723911b698a2c
marlon307 <marlon-ramosb@hotmail.com>
2022-12-08 16:11:07 -0300

    Start bag an account transition next app

---
+++ app/bag/page.tsx
@@ -0,0 +1,81 @@
+import React from 'react';
+// import { cookies } from 'next/headers';
+// import { SWRConfig } from 'swr';
+import { api2 } from '../../service/api';
+import ContentBag from '../../components/Bag/CompBag';
+import HeadSEO from '../../components/Head/HeadSEO';
+// import type { TypeAddBagInfos } from '../../@types/bag';
+
+// interface Props {
+//   fallback: {
+//     list_b: TypeAddBagInfos[];
+//   }
+// }
+
+async function getInfoBag() {
+  // console.log(cookies().get('u_token'));
+
+  const { data } = await api2.get('/bag', {
+    headers: {
+      authorization: `Bearer ${'req.cookies.u_token'}`,
+    },
+  }).catch((err) => ({ data: err }));
+  return {
+    props: {
+      fallback: {
+        '/bag': {
+          token: 'req.cookies.u_token',
+          infobag: data.infobag,
+        },
+      },
+    },
+  };
+}
+
+async function Bag() {
+  // console.log(cookies().get('u_token'));
+
+  const { props } = await getInfoBag();
+
+  return (
+    <>
+      <HeadSEO
+        title="Sacola e Checkout"
+        description="Finalize sua compra"
+      />
+      {/* <SWRConfig value={ { fallback } }> */ }
+      <ContentBag fallback={ props.fallback } />
+      {/* </SWRConfig> */ }
+    </>
+  );
+}
+
+export default Bag;
+
+// export const getServerSideProps: GetServerSideProps = async ({ req }) => {
+//   if (req.cookies.u_token) {
+//     const { data } = await api2.get('/bag', {
+//       headers: {
+//         authorization: `Bearer ${req.cookies.u_token}`,
+//       },
+//     }).catch((err) => ({ data: err }));
+
+//     return {
+//       props: {
+//         fallback: {
+//           '/bag': {
+//             token: req.cookies.u_token,
+//             infobag: data.infobag,
+//           },
+//         },
+//       },
+//     };
+//   }
+
+//   return {
+//     redirect: {
+//       permanent: false,
+//       destination: '/',
+//     },
+//   };
+// };
