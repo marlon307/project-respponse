aca700a997621a047555712d08faf976d9ea1130
marlon307 <marlon-ramosb@hotmail.com>
2022-12-14 16:20:05 -0300

    Head account

---
+++ app/account/head.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export default async function Head() {
+  return (
+    <>
+      <title>Respponse - Conta</title>
+      <meta name="description" content="Conta do usuário" />
+      <link rel="canonical" href="https://project-respponse-marlon307.vercel.app/account" />
+      <meta property="og:title" content="Respponse - Conta" />
+      <meta property="og:description" content="Respponse,pedidos,endereço,Configurações do Usuário, ajuda" />
+      <meta name="keywords" content="Respponse, pedidos, endereço, Configurações do Usuário, ajuda" />
+    </>
+  );
+}
