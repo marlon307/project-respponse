7c946c91b2a08b26f627d06c5c9359e1d86b9450
marlon307 <marlon-ramosb@hotmail.com>
2022-12-14 11:40:45 -0300

    Add file head product id

---
+++ app/product/[id]/head.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import { notFound } from 'next/navigation';
+import { api2 } from '../../../service/api';
+import type { Props, TypeProduct } from './product';
+
+async function getProductID(prodID: number): Promise<TypeProduct> {
+  const { data } = await api2.get(`/product/${prodID}`)
+    .catch(() => notFound());
+  return data.product;
+}
+
+export default async function Head({ params }: Props) {
+  const product = await getProductID(params.id);
+
+  return (
+    <>
+      <title>{ `${product.category_name} - ${product.title}` }</title>
+      <meta name="description" content={ product.descrtion } />
+      <link rel="canonical" href={ `https://project-respponse-marlon307.vercel.app${product.id}` } />
+      <meta property="og:title" content={ product.title } />
+      <meta property="og:description" content={ product.descrtion } />
+      <meta name="keywords" content={ `${product.category_name} - ${product.title}` } />
+    </>
+  );
+}
