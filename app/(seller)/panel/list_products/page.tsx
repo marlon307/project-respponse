3b12e9429e01eb0a6685f753a50b22af57b0d4c8
marlon307 <marlon-ramosb@hotmail.com>
2023-01-06 13:28:08 -0300

    ender products panel seller

--- app/(seller)/panel/list_products/page.tsx
+++ app/(seller)/panel/list_products/page.tsx
@@ -1,9 +1,40 @@
+import { cookies } from 'next/headers';
 import React from 'react';
+import CardProduct from '../../../../components/Cards/CardProduct/CardProduct';
+import { api2 } from '../../../../service/api';
+import type { PropsCardProductCategory } from '../../../../@types/typeCardProduct';
+import style from './style.module.scss';
 
-function Page() {
+interface Props {
+  status: number;
+  detail: string;
+  products: PropsCardProductCategory[]
+}
+
+async function getListProductsSeller(): Promise<Props> {
+  const cookie = cookies().get('u_token')?.value;
+  const { data } = await api2.get('/seller/products', {
+    headers: {
+      authorization: `Bearer ${cookie}`,
+    },
+  });
+  return data;
+}
+
+async function Page() {
+  const { products } = await getListProductsSeller();
+
   return (
-    <div>
-      <h1>Clicque em um para editar</h1>
+    <div className={ style.products_seller }>
+      <h1>Clique em um para editar</h1>
+      <div className={ style.container_products }>
+        { products.map((product) => (
+          <CardProduct
+            key={ product.id }
+            objectProduct={ product }
+          />
+        )) }
+      </div>
     </div>
   );
 }
