0bba1af241097b598bd8fb04bd8b41e4c1d87f04
marlon307 <marlon-ramosb@hotmail.com>
2023-01-14 13:58:11 -0300

    url head

--- app/bag/head.tsx
+++ app/bag/head.tsx
@@ -4,13 +4,13 @@ import DefaultTags from '../../components/Head/DefaultTags';
 export default async function Head() {
   return (
     <>
-      <DefaultTags />
       <title>Sacola de Compras - Respponse</title>
       <meta name="description" content="Finalize a compra da sacola aqui." />
-      <link rel="canonical" href="https://project-respponse-marlon307.vercel.app/" />
+      <link rel="canonical" href="https://project-respponse-marlon307.vercel.app/bag" />
       <meta property="og:title" content="Sacola de Compras - Respponse" />
       <meta property="og:description" content="inalize a compra da sacola aqui." />
       <meta name="keywords" content="Respponse,sacola,bag,checkout" />
+      <DefaultTags />
     </>
   );
 }
